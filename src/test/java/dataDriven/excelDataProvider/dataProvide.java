370ec75824ff75091aba0dbad29b8fa2091527f0
Greg <gregsmith446@gmail.com>
2019-11-25 20:27:58 -0500

    removed comments + remade excel test with better naming convention

--- src/test/java/dataDriven/excelDataProvider/dataProvide.java
+++ src/test/java/dataDriven/excelDataProvider/dataProvide.java
@@ -25,21 +25,14 @@ public class dataProvide {
 	@DataProvider(name="driveTest")
 	public Object[][] getData() throws IOException
 	{	
-		// every row of excel = 1 array in multi-dimensional array
 		FileInputStream fis = new FileInputStream("C:\\Users\\gregs\\eclipse-workspace\\excelDataProvider\\excelDriven.xlsx"); 
-		// excel instance, takes the excel file as an arg
 		XSSFWorkbook wb = new XSSFWorkbook(fis);
-		// return type of below method is XSSF sheet
 		XSSFSheet sheet = wb.getSheetAt(0);
-		// how to get the # of rows
 		int rowCount = sheet.getPhysicalNumberOfRows();
 		XSSFRow row = sheet.getRow(0);	
-		// how to get columnCount (the last cell in a row!)
 		int colCount = row.getLastCellNum();
-		// declare multi-dimensional array with above variables 
 		Object data [][] = new Object[rowCount - 1][colCount];
 			
-		// Object[][] data = { {"hello", "text", "1"}, {"bye", "message", "143"}, {"solo", "call", "453"} };
 		for (int i = 0; i < rowCount - 1; i++)
 		{
 			row = sheet.getRow(i + 1);			
@@ -53,6 +46,7 @@ public class dataProvide {
 	}
 }
 
+// Object[][] data = { {"hello", "text", "1"}, {"bye", "message", "143"}, {"solo", "call", "453"} };
 
 
 
--- src/test/java/dataDriven/excelDataProvider/excel.java
+++ src/test/java/dataDriven/excelDataProvider/excel.java
@@ -1,7 +1,6 @@
 package dataDriven.excelDataProvider;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -15,16 +14,11 @@ public class excel {
 	public void getExcel() throws IOException
 	{
 		FileInputStream fis = new FileInputStream("C:\\Users\\gregs\\eclipse-workspace\\excelDataProvider\\excelDriven.xlsx"); 
-		// excel instance, takes the excel file as an arg
 		XSSFWorkbook wb = new XSSFWorkbook(fis);
-		// return type of below method is XSSF sheet
 		XSSFSheet sheet = wb.getSheetAt(0);
-		// how to get the # of rows
 		int rowCount = sheet.getPhysicalNumberOfRows();
 		XSSFRow row = sheet.getRow(0);	
-		// how to get columnCount (the last cell in a row!)
 		int colCount = row.getLastCellNum();
-		// declare multi-dimensional array with above variables 
 		Object Data [][] = new Object[rowCount - 1][colCount];
 		
 		for (int i = 0; i < rowCount; i++)
